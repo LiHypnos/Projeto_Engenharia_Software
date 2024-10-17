03300f9282c56928b1a4bb2d71d297f326959c27
Lian <elianfernandosc@gmail.com>
2024-10-17 10:44:59 -0300

    Arrumando Conflitos

--- D.java
+++ D.java
@@ -36,11 +36,7 @@ public class D {
     }
 
     public void MD3() {
-<<<<<<< HEAD
     	System.out.println("Método MD3");
-=======
-        System.out.println("Método MD3");
->>>>>>> NewFeatures_Joao_NewOP
     }
     
     public void MD4() {
